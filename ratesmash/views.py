a30159c301b4734bafeb3340e383c34634e197e3
beginner <python.beginner.smashai@gmail.com>
2018-10-30 00:54:27 +0900

    Changed the views.py for the site.

--- ratesmash/views.py
+++ ratesmash/views.py
@@ -241,12 +241,12 @@ def process_rate(room_id):
                     player1.playable = True
                     player1.save()
                 else:
-                    player1.rate -= 16 + (rate2 - rate1) * 0.04
+                    player1.rate -= 16 + (rate1 - rate2) * 0.04
                     player1.rate = int(player1.rate)
                     player1.playable = True
                     player1.save()
                 if winloss2 == True:
-                    player2.rate += 16 + (rate1 - rate2) * 0.04
+                    player2.rate += 16 + (rate2 - rate1) * 0.04
                     player2.rate = int(player2.rate)
                     player2.playable = True
                     player2.save()
